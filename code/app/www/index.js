e2d629e4481a4ad9bb3e1c82dda4253f0a9acc5c
Marek Labos <marek@specle.net>
2015-06-20 20:07:02 +0100

    Show screenshot before loading real model

--- code/app/www/index.js
+++ code/app/www/index.js
@@ -1,29 +1,8 @@
-/////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) Autodesk, Inc. All rights reserved
-// Written by Philippe Leefsma 2014 - ADN/Developer Technical Services
-//
-// Permission to use, copy, modify, and distribute this software in
-// object code form for any purpose and without fee is hereby granted,
-// provided that the above copyright notice appears in all copies and
-// that both that copyright notice and the limited warranty and
-// restricted rights notice below appear in all supporting
-// documentation.
-//
-// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
-// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
-// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
-// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
-// UNINTERRUPTED OR ERROR FREE.
-/////////////////////////////////////////////////////////////////////////////////
-var defaultUrn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6aW5kYWhvdXNlL1NhbXBlSG91c2UzRC5kd2c';
-
-$(document).ready(function () {
+function loadModel(viewer_holder, urn) {
     var tokenurl = 'http://' + window.location.host + '/api/token';
     var config = {
         environment : 'AutodeskProduction',
-        version: '1.2.15'
-		//environment : 'AutodeskStaging'
-        //
+        //environment : 'AutodeskStaging'
     };
 
     // Instantiate viewer factory
@@ -31,24 +10,30 @@ $(document).ready(function () {
         tokenurl,
         config);
 
-    // Allows different urn to be passed as url parameter
-    var paramUrn = Autodesk.Viewing.Private.getParameterByName('urn');
-    var urn = (paramUrn !== '' ? paramUrn : defaultUrn);
-
     viewerFactory.getViewablePath(urn,
         function(pathInfoCollection) {
-            var viewerConfig = {
-                viewerType: 'GuiViewer3D'
-            };
+            var viewer,
+                viewerConfig = {
+                    viewerType: 'GuiViewer3D',
+                    version: '1.2.15'
+                };
 
-            var viewer = viewerFactory.createViewer(
-                document.getElementById('viewer-container'),
+            viewer_holder.empty();
+
+            viewer = viewerFactory.createViewer(
+                viewer_holder.get(0),
                 viewerConfig);
 
             viewer.load(pathInfoCollection.path3d[0].path);
         },
         console.log);
+}
 
+$(document).ready(function () {
+    var defaultUrn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6aW5kYWhvdXNlL1NhbXBlSG91c2UzRC5kd2c',
+        holder = $('#viewer-container');
+
+    loadModel(holder, defaultUrn);
 });
 
 
